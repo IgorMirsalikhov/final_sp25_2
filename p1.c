16f212ce003e65b2f9af57fbb3aa5786ce7d74a1
Igor Mirsalikhov <imirsalikhov@floridapoly.edu>
2025-04-30 19:28:24 -0400

    Create p1.c

---
+++ p1.c
@@ -0,0 +1,12 @@
+void getString(char *str);
+
+void getString(char *str) {
+    int i = 0;
+    char c = getchar(); 
+    while (i < SIZE - 1 && c != '\n') {
+        str[i] = c;
+        i++;
+        c = getchar();
+    }
+    str[i] = '\0';
+}
